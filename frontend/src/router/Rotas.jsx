c5241ff3dd8077b9647a526c0219b32d171b4fb5
EmersonRolemberg <115815559+EmersonRolemberg@users.noreply.github.com>
2023-03-30 18:50:14 -0300

    Add files via upload

---
+++ frontend/src/router/Rotas.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Acessorios from "../pages/Acessorios/Acessorios";
+import CadastrarAcessorios from "../pages/CadastrarAcessorios/CadastrarAcessorios"
+import DeletarAcessorios from "../pages/DeletarAcessorios/DeletarAcessorios";
+import AtualizarAcessorios from "../pages/AtualizarAcessorios/AtualizarAcessorios";
+
+const Rotas = () => {
+  return (
+    <Routes>
+      <Route path="/acessorios" element={<Acessorios/>} />
+      <Route path="/acessorios/cadastro" element={<CadastrarAcessorios/>} />
+      <Route path="/acessorios/deletar" element={<DeletarAcessorios/>} />
+      <Route path="/acessorios/atualizar" element={<AtualizarAcessorios/>} />
+    </Routes>
+  );
+};
+
+export default Rotas;
